dd72943deec17a3be4564d288813857f7cb1b958
dyuki <unblivion@gmail.com>
2018-08-10 18:49:55 -0300

    menu hamburguer 70% (falta deixar bonito)

--- inicial/script.js
+++ inicial/script.js
@@ -14,12 +14,20 @@ let span = document.getElementsByClassName("close")[0];
 span.onclick = function () {
     modal.style.display = "none";
 }
-// -----
-
+// --------------------
+//HAMBURGUER FUNFA
 let menuHamburguer = document.querySelector(".hamburguer");
 let menuContent = document.querySelector(".conteudoMenu");
+let menuInfo = document.querySelector(".informacoes");
+let infoContent = document.querySelector(".conteudoInformacao");
 
 menuHamburguer.onclick = function() {
     menuHamburguer.classList.toggle("mudar");
     menuContent.classList.toggle("opcoesMenu");
+    infoContent.classList.remove("opcoesMenuInfo");
 }
+
+menuInfo.onclick = function() {
+    infoContent.classList.toggle("opcoesMenuInfo");
+}
+//--------------------
