757e4188dead08f65a6bcb65c38275d024a5f171
Fabiano <fabiano.novais.99@gmail.com>
2018-08-20 21:20:47 -0300

    teste parallax mobile jquery

--- news/index.js
+++ news/index.js
@@ -189,3 +189,61 @@ let span9 = document.getElementsByClassName("close")[8];
 span9.onclick = function () {
     modal9.style.display = "none";
 }
+
+// _____________________________________JQuerry Parralax__________________
+$(function(){
+	$(window).scroll(function(){
+		var window_scrolltop = $(this).scrollTop();
+		
+		// Passa por cada elemento com a classe .parallax
+		$('.parallax1').each(function(){
+			var obj = $(this);
+			
+			// Garante que apenas trabalhemos no elemento que está visível na tela
+			if ( window_scrolltop >= obj.position().top - obj.height() 
+				&& window_scrolltop <= obj.position().top + obj.height()) {
+				
+				// O atributo data-divisor vai definir a velocidade do efeito
+				var divisor = typeof obj.attr('data-divisor') == 'undefined' ? 4 : obj.attr('data-divisor');
+				
+				// Corrige a diferença do primeiro elemento
+				if ( obj.is(':first-child') ) {
+					var bg_pos = ( window_scrolltop - obj.position().top ) / divisor;
+				} else {
+					var bg_pos = ( window_scrolltop - obj.position().top + ( obj.height() ) ) / divisor;
+				}
+				
+				// Modifica a posição do bg
+				obj.css({
+					'background-position' : '50% -' + bg_pos + 'px'
+				});
+				
+			} // Garante que apenas trabalhemos no elemento que está visível na tela
+        });
+        
+        $('.parallax3').each(function(){
+			var obj = $(this);
+			
+			// Garante que apenas trabalhemos no elemento que está visível na tela
+			if ( window_scrolltop >= obj.position().top - obj.height() 
+				&& window_scrolltop <= obj.position().top + obj.height()) {
+				
+				// O atributo data-divisor vai definir a velocidade do efeito
+				var divisor = typeof obj.attr('data-divisor') == 'undefined' ? 4 : obj.attr('data-divisor');
+				
+				// Corrige a diferença do primeiro elemento
+				if ( obj.is(':first-child') ) {
+					var bg_pos = ( window_scrolltop - obj.position().top ) / divisor;
+				} else {
+					var bg_pos = ( window_scrolltop - obj.position().top + ( obj.height() ) ) / divisor;
+				}
+				
+				// Modifica a posição do bg
+				obj.css({
+					'background-position' : '50% -' + bg_pos + 'px'
+				});
+				
+			} // Garante que apenas trabalhemos no elemento que está visível na tela
+		});// $('.parallax').each(function(){ ...
+	}); // $(window).scroll(function(){ ...
+}); // jQuery
